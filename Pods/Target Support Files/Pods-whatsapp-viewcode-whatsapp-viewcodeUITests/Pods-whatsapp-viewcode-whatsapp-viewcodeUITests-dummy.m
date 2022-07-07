568516695a38ecf82a23e17e5f9e90f7f6146d4b
Rogério Tavares <rogerioctf@gmail.com>
2022-07-07 06:23:17 -0300

    removendo pacotes

--- Pods/Target Support Files/Pods-whatsapp-viewcode-whatsapp-viewcodeUITests/Pods-whatsapp-viewcode-whatsapp-viewcodeUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_whatsapp_viewcode_whatsapp_viewcodeUITests : NSObject
-@end
-@implementation PodsDummy_Pods_whatsapp_viewcode_whatsapp_viewcodeUITests
-@end
